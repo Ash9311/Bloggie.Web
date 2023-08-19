e41fd38caca4e4e6ccb6e91d6f1dd94f19694595
@Ash9311 <ashutoshmulki7@gmail.com>
2023-08-19 23:15:46 +0530

    Add project files.

---
+++ Data/BloggieDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bloggie.Web.Models.domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> MyProperty { get; set; }
+    }
+}
